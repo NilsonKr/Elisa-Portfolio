88750e06defcd0e7892b4741848925aa6215a10e
NilsonKr <nuotrok444@gmail.com>
2021-02-28 01:15:48 -0500

    Last little details Done

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 // const trucazo = document.querySelector('#trucazo')
 const form = document.querySelector('#form')
+const links = document.querySelectorAll('.linkMenu')
+console.log(links)
 
 form.addEventListener('submit', handleSubmit)
 
@@ -9,3 +11,13 @@ function handleSubmit(ev) {
   trucazo.setAttribute('href', `mailto:me@nilsonkr.com?subject=${form.get('name')}${form.get('email')}&body=${form.get('message')}`)
   trucazo.click()
 }
+
+document.body.addEventListener('click', ev => {
+  const target = ev.target
+
+  
+  if(target.classList.contains('linkMenu')){
+    links.forEach( link => link.style.color = 'rgba(15, 10, 10, 0.6)')
+    target.style.color = '#0F0A0A'
+  }
+})
